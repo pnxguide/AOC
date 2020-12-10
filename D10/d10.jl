27e339cf6a2e3cc0f854beb37d2e24e4efa2d2d9
PnXGUiDE <yuttapichai.ke@gmail.com>
2020-12-10 15:20:11 +0700

    add day 10

---
+++ D10/d10.jl
@@ -0,0 +1,51 @@
+function solve1()
+    lines = readlines("input.txt")
+    numbers = [parse(Int, x) for x in lines]
+    sorted = sort(numbers)
+    sorted = prepend!(sorted, 0)
+    sorted = push!(sorted, sorted[size(sorted)[1]] + 3)
+
+    ones = 0
+    threes = 0
+
+    for i = 2:size(sorted)[1]
+        diff = sorted[i] - sorted[i - 1]
+
+        if diff == 1
+            ones += 1
+        elseif diff == 3
+            threes += 1
+        end
+    end
+
+    println(ones * threes)
+end
+
+function solve2()
+    lines = readlines("input.txt")
+    numbers = [parse(Int, x) for x in lines]
+    sorted = sort(numbers)
+    sorted = prepend!(sorted, 0)
+    sorted = push!(sorted, sorted[size(sorted)[1]] + 3)
+
+    ones = 0
+    arrangement = 1
+
+    multiple = [1, 2, 4, 7, 13]
+
+    for i = 2:size(sorted)[1]
+        diff = sorted[i] - sorted[i - 1]
+
+        if diff == 1
+            ones += 1
+        elseif diff == 3 && ones > 0
+            arrangement *= multiple[ones]
+            ones = 0
+        end
+    end
+
+    println(arrangement)
+end
+
+solve1()
+solve2()
