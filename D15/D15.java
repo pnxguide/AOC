3ccec9927468fa48b8bd62bd93459dc234933e88
PnXGUiDE <yuttapichai.ke@gmail.com>
2020-12-15 20:07:17 +0700

    update day 15

--- D15/D15.java
+++ D15/D15.java
@@ -3,25 +3,18 @@ import java.util.*;
 public class D15 {
     public static void main(String[] args) {
         int[] starts = {0,1,4,13,15,12,16};
-        Map<Integer, Integer> last = new HashMap<>();
         Map<Integer, Integer> prev = new HashMap<>();
 
         for(int i = 0; i < starts.length; i++) {
-            last.put(starts[i], i + 1);
+            prev.put(starts[i], i + 1);
         }
 
-        int cur = starts[starts.length - 1];
-        for(int i = starts.length + 1; i <= 30000000; i++) {
-            if(prev.get(cur) == null) {
-                cur = 0;
-                prev.put(cur, last.get(cur));
-            } else {
-                cur = last.get(cur) - prev.get(cur);
-                if(last.get(cur) != null) {
-                    prev.put(cur, last.get(cur));
-                }
-            }
-            last.put(cur, i);
+        int cur = 0;
+        int diff = 0;
+        for(int i = starts.length + 1; i < 30000000; i++) {
+            diff = prev.get(cur) == null ? 0 : i - prev.get(cur);
+            prev.put(cur, i);
+            cur = diff;
         }
 
         System.out.println(cur);
