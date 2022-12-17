2076a1656127a67f3002d4688acee6a1903c1b8e
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2022-12-17 02:55:46 -0500

    updated day17

---
+++ 2022/day16/solution.cc
@@ -0,0 +1,405 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+#include <vector>
+#include <deque>
+#include <set>
+#include <unordered_map>
+#include <algorithm>
+
+#define BOUND 26
+
+void tokenize(const std::string &s, const char delim,
+            std::vector<std::string> &out)
+{
+    std::string::size_type beg = 0;
+    for (auto end = 0; (end = s.find(delim, end)) != std::string::npos; ++end)
+    {
+        out.push_back(s.substr(beg, end - beg));
+        beg = end + 1;
+    }
+ 
+    out.push_back(s.substr(beg));
+}
+
+// Map node with ID
+std::unordered_map< std::string, int > V_map;
+std::unordered_map< int, std::string > map_V;
+std::vector<int> V(50, 0);
+std::vector< std::set<int> > adj(50);
+std::vector<int> reverse_nonzero_map(50);
+std::vector<int> nonzero_map;
+int nnz = 0;
+
+std::vector< std::vector<int> > shortest(50, std::vector<int>(50));
+
+std::unordered_map<uint64_t, int> recursion_A_table;
+
+uint64_t hash(int a, int b, std::set<int> c)
+{
+  // 11 bits
+  uint64_t h = (a * nnz + reverse_nonzero_map[b]);
+
+  // c
+  // 15 bits
+  h <<= nnz;
+  for (int cc : c)
+  {
+    h |= (0b1 << reverse_nonzero_map[cc]);
+  }
+
+  return h;
+}
+
+int recursion_A(int current_time, int current_node, std::set<int> is_visited)
+{
+  // Cache
+  int h = hash(current_time, current_node, is_visited);
+  int cached = recursion_A_table[h];
+  if (cached != 0)
+  {
+    return cached;
+  }
+
+  is_visited.insert(current_node);
+
+  if (current_time >= BOUND) return 0;
+  else
+  {
+    // Calculate yields and use the maximum one
+    int max_yield = 0;
+    for (int k = 1; k < nnz; k++)
+    {
+      int i = nonzero_map[k];
+      if (is_visited.find(i) != is_visited.end()) continue;
+
+      int yield = ((BOUND - current_time) - shortest[current_node][i] - 1) * V[i];
+      if (yield > 0)
+      {
+        max_yield = std::max(max_yield, yield + recursion_A(current_time + shortest[current_node][i] + 1, i, is_visited));
+      }
+    }
+
+    recursion_A_table[h] = max_yield;
+    return max_yield;
+  }
+}
+
+uint64_t hashB(int a, int b, int x, int y, std::set<int> c)
+{
+  // 11 bits
+  uint64_t h = ((a * nnz + reverse_nonzero_map[b]) * (BOUND * nnz)) + (x * nnz + reverse_nonzero_map[y]);
+
+  // c
+  // 15 bits
+  h <<= nnz;
+  for (int cc : c)
+  {
+    h |= (0b1 << reverse_nonzero_map[cc]);
+  }
+
+  return h;
+}
+
+std::unordered_map<uint64_t, int> recursion_B_table;
+
+int recursion_B(int current_time1, int current_time2, int current_node1, int current_node2, std::set<int> is_visited)
+{
+  uint64_t h = hashB(current_time1, current_time2, current_node1, current_node2, is_visited);
+  int cached = recursion_B_table[h];
+  if (cached != 0)
+  {
+    return cached;
+  }
+
+  is_visited.insert(current_node1);
+  is_visited.insert(current_node2);
+
+  if (current_time1 >= BOUND && current_time2 >= BOUND) 
+  {
+    recursion_B_table[h] = 0;
+    return 0;
+  }
+  else
+  {
+    // Calculate yields and use the maximum one
+    int max_yield = 0;
+
+    std::vector<int> yields1(nnz);
+    std::vector<int> yields2(nnz);
+    std::vector<int> yields1_list;
+    std::vector<int> yields2_list;
+    
+    for (int i = 1; i < nnz; i++)
+    {
+      int x = nonzero_map[i];
+
+      if (is_visited.find(x) == is_visited.end())
+      {
+        yields1[i] = ((BOUND - current_time1) - shortest[current_node1][x] - 1) * V[x];
+        yields2[i] = ((BOUND - current_time2) - shortest[current_node2][x] - 1) * V[x];
+
+        if (yields1[i] > 0)
+        {
+          yields1_list.push_back(i);
+          // std::cout << "1: " << map_V[x] << std::endl;
+        }
+
+        if (yields2[i] > 0) 
+        {
+          yields2_list.push_back(i);
+          // std::cout << "2: " << map_V[x] << std::endl;
+        }
+      }
+    }
+
+    for (int x : yields1_list)
+    {
+      int i = nonzero_map[x];
+      int yield1 = yields1[x];
+
+      for (int y : yields2_list)
+      {
+        int j = nonzero_map[y];
+        int yield2 = yields2[y];
+
+        // Expand two directions
+        if (i != j)
+        {
+          std::cout << map_V[i] << " (" << current_time1 << ") / " << map_V[j] << " (" << current_time2 << ")" << std::endl;
+          // std::cout << map_V[i] << " (" << shortest[current_node1][i] << ") / " << map_V[j] << " (" << shortest[current_node2][j] << ")" << std::endl;
+          std::cout << "Yield: " << yield1 + yield2 << " (" << yield1 << " + " << yield2 << ")" << std::endl;
+
+          max_yield = std::max(
+            max_yield, 
+            yield1 + yield2 + recursion_B(
+              current_time1 + shortest[current_node1][i] + 1,
+              current_time2 + shortest[current_node2][j] + 1,
+              i, j, is_visited));
+        }
+
+        // Expand only one direction
+        // if ()
+        {
+          std::cout << "(1st) " << map_V[i] << " (" << current_time1 << ")" << std::endl;
+          std::cout << "Yield: " << yield1 << std::endl;
+
+          max_yield = std::max(
+            max_yield, 
+            yield1 + recursion_A(
+              current_time1 + shortest[current_node1][i] + 1,
+              i, is_visited));
+        }
+
+        // if (which_case == -1 || which_case == 2)
+        {
+          std::cout << "(2nd) " << map_V[j] << " (" << current_time2 << ")" << std::endl;
+          std::cout << "Yield: " << yield2 << std::endl;
+
+          max_yield = std::max(
+            max_yield, 
+            yield2 + recursion_A(
+              current_time2 + shortest[current_node2][j] + 1,
+              j, is_visited));
+        }
+      }
+    }
+
+    recursion_B_table[h] = max_yield;
+    return max_yield;
+  }
+}
+
+void solve_A()
+{
+  // Read an input file
+  std::ifstream input_file("input.txt");
+  std::string line;
+
+  int count = 0;
+  while (std::getline(input_file, line))
+  {
+    std::vector< std::string > tokens;
+    tokenize(line, ' ', tokens);
+    if (V_map.find(tokens[1]) == V_map.end())
+    {
+      map_V[count] = tokens[1];
+      V_map[tokens[1]] = count++;
+    }
+
+    std::vector< std::string > subtokens;
+    tokenize(tokens[4], '=', subtokens);
+    V[V_map[tokens[1]]] = std::stoi(subtokens[1]);
+
+    if (V[V_map[tokens[1]]] != 0)
+    {
+      nonzero_map.push_back(V_map[tokens[1]]);
+      reverse_nonzero_map[V_map[tokens[1]]] = nnz++;
+    }
+
+    for (size_t i = 9; i < tokens.size(); i++)
+    {
+      if (i != tokens.size() - 1) 
+      {
+        tokens[i].pop_back();
+      }
+      
+      if (V_map.find(tokens[i]) == V_map.end())
+      {
+        map_V[count] = tokens[i];
+        V_map[tokens[i]] = count++;
+        
+      }
+
+      adj[V_map[tokens[1]]].insert(V_map[tokens[i]]);
+    }
+  }
+
+  for (int k = 0; k < count; k++)
+  {
+    std::set<int> b, m, c;
+    std::vector<int> lengths(50, 999);
+    b.insert(k);
+    m.insert(k);
+
+    for (int i = 0; i < BOUND; i++)
+    {
+      if (b.size() == 0) break;
+
+      // Update lengths
+      for (int s : b)
+      {
+        shortest[k][s] = i;
+      }
+
+      // c = Ab - m
+      // union all selected rows
+      for (int b_i : b)
+      {
+        // Union
+        std::set<int> tmp;
+        std::set_union(c.begin(), c.end(), adj[b_i].begin(), adj[b_i].end(),
+          std::inserter(tmp, tmp.end()));
+        c = tmp;
+      }
+      // Difference
+      b.clear();
+      std::set_difference(c.begin(), c.end(), m.begin(), m.end(),
+        std::inserter(b, b.end()));
+      // Update m
+      std::set<int> tmp;
+      std::set_union(m.begin(), m.end(), b.begin(), b.end(),
+        std::inserter(tmp, tmp.end()));
+      m = tmp;
+    }
+  }
+
+  // Compute SSSP from the current node
+  // Using BFS
+  int current_node = V_map["AA"];
+
+  std::set<int> is_visited;
+  is_visited.insert(current_node);
+
+  std::cout << recursion_A(0, V_map["AA"], is_visited) << std::endl;
+}
+
+void solve_B()
+{
+  // Read an input file
+  std::ifstream input_file("input.txt");
+  std::string line;
+
+  int count = 0;
+  while (std::getline(input_file, line))
+  {
+    std::vector< std::string > tokens;
+    tokenize(line, ' ', tokens);
+    if (V_map.find(tokens[1]) == V_map.end())
+    {
+      map_V[count] = tokens[1];
+      V_map[tokens[1]] = count++;
+    }
+
+    std::vector< std::string > subtokens;
+    tokenize(tokens[4], '=', subtokens);
+    V[V_map[tokens[1]]] = std::stoi(subtokens[1]);
+
+    if (V[V_map[tokens[1]]] != 0 || tokens[1] == "AA")
+    {
+      nonzero_map.push_back(V_map[tokens[1]]);
+      reverse_nonzero_map[V_map[tokens[1]]] = nnz++;
+    }
+
+    for (size_t i = 9; i < tokens.size(); i++)
+    {
+      if (i != tokens.size() - 1) 
+      {
+        tokens[i].pop_back();
+      }
+      
+      if (V_map.find(tokens[i]) == V_map.end())
+      {
+        map_V[count] = tokens[i];
+        V_map[tokens[i]] = count++;
+        
+      }
+
+      adj[V_map[tokens[1]]].insert(V_map[tokens[i]]);
+    }
+  }
+
+  for (int k = 0; k < count; k++)
+  {
+    std::set<int> b, m, c;
+    std::vector<int> lengths(50, 999);
+    b.insert(k);
+    m.insert(k);
+
+    for (int i = 0; i < BOUND; i++)
+    {
+      if (b.size() == 0) break;
+
+      // Update lengths
+      for (int s : b)
+      {
+        shortest[k][s] = i;
+      }
+
+      // c = Ab - m
+      // union all selected rows
+      for (int b_i : b)
+      {
+        // Union
+        std::set<int> tmp;
+        std::set_union(c.begin(), c.end(), adj[b_i].begin(), adj[b_i].end(),
+          std::inserter(tmp, tmp.end()));
+        c = tmp;
+      }
+      // Difference
+      b.clear();
+      std::set_difference(c.begin(), c.end(), m.begin(), m.end(),
+        std::inserter(b, b.end()));
+      // Update m
+      std::set<int> tmp;
+      std::set_union(m.begin(), m.end(), b.begin(), b.end(),
+        std::inserter(tmp, tmp.end()));
+      m = tmp;
+    }
+  }
+
+  int current_node = V_map["AA"];
+
+  std::set<int> is_visited;
+  is_visited.insert(current_node);
+
+  std::cout << recursion_B(0, 0, V_map["AA"], V_map["AA"], is_visited) << std::endl;
+}
+
+int main()
+{
+  // solve_A();
+  solve_B();
+  return 0;
+}
---
+++ 2022/day17/solution.cc
@@ -0,0 +1,257 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+#include <vector>
+#include <deque>
+#include <set>
+#include <unordered_map>
+#include <algorithm>
+
+#define LIMIT (1709 + 1581)
+
+void tokenize(const std::string &s, const char delim,
+            std::vector<std::string> &out)
+{
+    std::string::size_type beg = 0;
+    for (auto end = 0; (end = s.find(delim, end)) != std::string::npos; ++end)
+    {
+        out.push_back(s.substr(beg, end - beg));
+        beg = end + 1;
+    }
+ 
+    out.push_back(s.substr(beg));
+}
+
+void solve_A()
+{
+  // Read an input file
+  std::ifstream input_file("input.txt");
+  std::string commands;
+  input_file >> commands;
+
+  // Coordinate (x, -y)
+
+  std::vector< std::vector< std::pair<long, long> > > patterns(5);
+
+  // Pattern 1
+  patterns[0].push_back(std::make_pair(0, 0));
+  patterns[0].push_back(std::make_pair(1, 0));
+  patterns[0].push_back(std::make_pair(2, 0));
+  patterns[0].push_back(std::make_pair(3, 0));
+
+  // Pattern 2
+  patterns[1].push_back(std::make_pair(1, 0));
+  patterns[1].push_back(std::make_pair(0, 1));
+  patterns[1].push_back(std::make_pair(1, 1));
+  patterns[1].push_back(std::make_pair(2, 1));
+  patterns[1].push_back(std::make_pair(1, 2));
+
+  // Pattern 3
+  patterns[2].push_back(std::make_pair(0, 0));
+  patterns[2].push_back(std::make_pair(1, 0));
+  patterns[2].push_back(std::make_pair(2, 0));
+  patterns[2].push_back(std::make_pair(2, 1));
+  patterns[2].push_back(std::make_pair(2, 2));
+
+  // Pattern 4
+  patterns[3].push_back(std::make_pair(0, 0));
+  patterns[3].push_back(std::make_pair(0, 1));
+  patterns[3].push_back(std::make_pair(0, 2));
+  patterns[3].push_back(std::make_pair(0, 3));
+
+  // Pattern 5
+  patterns[4].push_back(std::make_pair(0, 0));
+  patterns[4].push_back(std::make_pair(1, 0));
+  patterns[4].push_back(std::make_pair(0, 1));
+  patterns[4].push_back(std::make_pair(1, 1));
+
+  long max_height = 0;
+  long count = 0;
+
+  long current_pattern = 0;
+  std::vector< std::pair<long, long> > current_position;
+  for (size_t i = 0; i < patterns[current_pattern].size(); i++)
+  {
+    current_position.push_back(
+      std::make_pair(
+        patterns[current_pattern][i].first + 2,
+        patterns[current_pattern][i].second + 3 + max_height));
+  }
+
+  std::vector< std::unordered_map<long, bool> > board(7, std::unordered_map<long, bool>());
+
+  while (count < LIMIT)
+  {
+    bool just_started = false;
+
+    for (size_t i = 0; i < commands.size(); i++)
+    {
+      just_started = false;
+
+      switch (commands[i])
+      {
+        case '>':
+        {
+          bool can_move_right = true;
+          for (size_t j = 0; j < current_position.size(); j++)
+          {
+            if (current_position[j].first >= 6 || board[current_position[j].first + 1][current_position[j].second])
+            {
+              can_move_right = false;
+              break;
+            }
+          }
+
+          if (can_move_right)
+          {
+            for (size_t j = 0; j < current_position.size(); j++)
+              current_position[j].first++;
+          }
+        }
+        break;
+        case '<':
+        {
+          bool can_move_left = true;
+          for (size_t j = 0; j < current_position.size(); j++)
+          {
+            if (current_position[j].first <= 0 || board[current_position[j].first - 1][current_position[j].second])
+            {
+              can_move_left = false;
+              break;
+            }
+          }
+
+          if (can_move_left)
+          {
+            for (size_t j = 0; j < current_position.size(); j++)
+              current_position[j].first--;
+          }
+        }
+        break;
+      }
+
+      bool can_move_down = true;
+      for (size_t j = 0; j < current_position.size(); j++)
+      {
+        // Check bottom
+        if (current_position[j].second <= 0 || board[current_position[j].first][current_position[j].second - 1])
+        {
+          can_move_down = false;
+          break;
+        }
+      }
+
+      if (can_move_down)
+      {
+        for (size_t j = 0; j < current_position.size(); j++)
+          current_position[j].second--;
+      }
+
+      // Change shape
+      else
+      {
+        // 1) Put shape in the board
+        for (size_t j = 0; j < current_position.size(); j++)
+        {
+          board[current_position[j].first][current_position[j].second] = true;
+          if (current_position[j].second > max_height)
+          {
+            max_height = current_position[j].second;
+          }
+        }
+
+        // 2) Change the shape
+        count += 1;
+        current_pattern = count % 5;
+        // if (count % 100000 == 0) std::cout << count << std::endl;
+
+        // 3) Generate new current_position
+        current_position.clear();
+        for (size_t j = 0; j < patterns[current_pattern].size(); j++)
+        {
+          current_position.push_back(
+            std::make_pair(
+              patterns[current_pattern][j].first + 2, 
+              patterns[current_pattern][j].second + 3 + max_height + 1));
+        }
+
+        just_started = true;
+      }
+
+      if (count >= LIMIT)
+      {
+        break;
+      }
+    }
+
+    for (size_t i = 0; i < current_position.size(); i++)
+    {
+      std::cout << current_position[i].first << " " << current_position[i].second << std::endl;
+    }
+
+    for (size_t j = 0; j < 4; j++)
+    {
+      for (size_t i = 0; i < 7; i++)
+      {
+        std::cout << board[i][max_height-j];
+      }
+      std::cout << std::endl;
+    }
+
+    std::cout << count << " " << (max_height + 1) << std::endl;
+  }
+
+  // 1st Loop         = 2591          / #shape = 1709
+  // 2nd Loop         = 5173 (+2582)  / 3414 (+1705)
+  // 3rd Loop         = 7755 (+2582)  / 5119 (+1705)
+  // ...
+  // 586510262nd Loop = 1514369499075 (+2582) / 999999998419 (+1705)
+
+  // 1514369499075 + 2412
+  
+  // 1514369501487
+  // 1514369502363
+
+  // 100000th Shape = 151452
+
+  // 1st Loop         = 2591          / #shape = 1709
+  // 2nd Loop         = 5173 (+2582)  / 3414 (+1705)
+  // 3rd Loop         = 7755 (+2582)  / 5119 (+1705)
+  // ...
+  // 586510262nd Loop = 1514369499075 (+2582) / 999999998419 (+1705)
+
+  // 1709 + (1705 * 586510262)
+  // 586510262nd Loop makes 999999998419 shapes
+  // 2591 + (2582 * 586510262)
+  // 586510262nd Loop makes 1514369499075
+  // 1581 shapes left
+  // 
+
+  // 1709 + (1705 * n) = 100000
+  // n = 57
+
+  // 57th Loop makes 98894 shapes
+  // 57th Loop makes 2591 + (2582 * 57) = 149765
+  // 1106 shapes left
+  // 151454 - 2
+
+  // 150871
+
+  std::cout << (max_height + 1) << std::endl;
+}
+
+void solve_B()
+{
+  // Read an input file
+  std::ifstream input_file("input.txt");
+  std::string line;
+
+}
+
+int main()
+{
+  solve_A();
+  // solve_B();
+  return 0;
+}
